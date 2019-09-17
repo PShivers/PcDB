c12be0dfc95dd97a6ef588ad973fd59fd30ab76e
Paul Shivers <paulsshivers@gmail.com>
2019-09-17 19:47:18 -0400

    got react to show and fixed router issue on backend

---
+++ routes/userRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const usersController = require('../controllers/usersController.js');
+
+router.get('/users', usersController.index);
+router.post('/users', usersController.create);
+
+router.get('/users/:id', usersController.show);
+router.put('/users/:id', usersController.update);
+router.delete('/users/:id', usersController.delete);
+
+module.exports = router;
